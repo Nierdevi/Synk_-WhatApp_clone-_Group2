541c4a13349cc78429d82d0026344b50185bd085
Nierdevi <genistar8@gmail.com>
2024-06-10 14:50:33 +0000

    commit

--- Synk/app/(tabs)/_layout.tsx
+++ Synk/app/(tabs)/_layout.tsx
@@ -10,28 +10,29 @@ export default function TabLayout() {
 
   return (
     <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
-        }}
-      />
+      // screenOptions={{
+      //   tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+      //   headerShown: false,
+      // }}
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: 'Chats',
+            tabBarIcon: ({ color }) => (
+              <TabBarIcon name='home' color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="explore"
+          options={{
+            title: 'Explore',
+            tabBarIcon: ({ color }) => (
+              <TabBarIcon name='code-slash' color={color} />
+            ),
+          }}
+        />
     </Tabs>
   );
 }
