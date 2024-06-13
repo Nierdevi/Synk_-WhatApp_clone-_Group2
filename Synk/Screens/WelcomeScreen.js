de473b6b2c36226d15b5c7459c0f16ac0457d939
Nierdevi <genistar8@gmail.com>
2024-06-13 21:19:01 +0000

    added color in constant folder

--- Synk/Screens/WelcomeScreen.js
+++ Synk/Screens/WelcomeScreen.js
@@ -1,5 +1,6 @@
 import { View, Text,StyleSheet } from 'react-native'
 import React from 'react'
+import primaryColors from '../constants/colors'
 
 export default function WelcomeScreen() {
   return (
@@ -13,7 +14,8 @@ const styles=StyleSheet.create({
   container:{
     flex:1,
     justifyContent:'center',
-    alignItems:'center'
+    alignItems:'center',
+    backgroundColor:primaryColors.white
   },
   text:{
         fontSize:24,
---
+++ Synk/constants/colors.js
@@ -0,0 +1,9 @@
+
+// colors.js
+const primaryColors = {
+    purple: "#7410d7",
+    black: "#110f10",
+    white: "#f9fafb"
+  };
+
+export default primaryColors;
