5496d0104283ab18164c6b0132598c429c096d7b
Wills TONY <willstony177@gmail.com>
2024-06-16 02:53:53 -1200

    wills

---
+++ Synk/Supabase/supabase.js
@@ -0,0 +1,5 @@
+import { SupabaseClient } from '@supabase/supabase-js';
+
+const supabaseUrl = https//afdyyuzbvekzdlscywvy.supabase.co
+const anonKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFmZHl5dXpidmVremRsc2N5d3Z5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg1NDY4NTgsImV4cCI6MjAzNDEyMjg1OH0.Vr19vwvIWTd9GXAfZaYkveqcyUPBhf133ADmYdKAzQ8
+const supabase = new SupabaseClient(supabaseUrl, anonKey);
