3dfea6a51d69013486c1159c9217d537a593b1a9
Wills TONY <willstony177@gmail.com>
2024-06-16 03:35:41 -1200

    wills

--- Synk/App.js
+++ Synk/App.js
@@ -5,6 +5,8 @@ import WelcomeScreen from './Screens/WelcomeScreen';
 import { ThemeProvider } from './constants/themeContext';
 import { StyleSheet, View, Text } from 'react-native';
 import PhoneNumScreen from './Screens/PhoneNumScreen';
+import { supabase } from './supabase';
+
 
 // import Verification from './Screens/Verification';
 import SettingsScreen from './Screens/SettingsScreen';
