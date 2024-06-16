8c003ac28eebf0b4ea13d09e50f3ddb866bbcebb
Nierdevi <genistar8@gmail.com>
2024-06-16 15:11:44 +0000

    h

--- Synk/Screens/WelcomeScreen.js
+++ Synk/Screens/WelcomeScreen.js
@@ -1,6 +1,6 @@
 import { Entypo } from '@expo/vector-icons';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { StyleSheet, Text, View,Image, Pressable } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen';
 import { PopupMenu } from '../components/PopupMenu';
