315e50e520d8e564d840abc55baea21fd0c17229
Nierdevi <genistar8@gmail.com>
2024-07-02 13:52:27 +0000

    m

--- Synk/Screens/tabs/chats/ChatsStackNavigator .js
+++ Synk/Screens/tabs/chats/ChatsStackNavigator .js
@@ -17,11 +17,8 @@ const ChatsStackNavigator = ({navigation,route}) => {
   React.useLayoutEffect(() => {
     const routeName = getFocusedRouteNameFromRoute(route) ?? 'ChatsScreen';
     if (routeName === 'ChatsScreen') {
-<<<<<<< HEAD
       navigation.setOptions({ tabBarStyle: { display: 'flex',height:70 } });
-=======
       navigation.setOptions({ tabBarStyle: { display: 'flex',height:80 },headerShown:true },);
->>>>>>> 8035e89c037c9ef55245fc9444d0074e5a722d25
     } else {
       navigation.setOptions({ tabBarStyle: { display: 'none' },headerShown:false });
     }
